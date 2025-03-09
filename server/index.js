d4c78522823e1d8c20386ce3ac89d03ea897cc27
Anawin51 <anawin.yo@ku.th>
2025-03-09 12:59:55 +0700

    update

--- server/index.js
+++ server/index.js
@@ -20,6 +20,31 @@ const initMySQL = async () => {
         port: 8820
     })
 }
+
+const validateData = (userData) => {
+    let errors = []
+    
+    if (!userData.firstname) {
+        errors.push('กรุณากรอกชื่อ')
+      }
+    if (!userData.lastName) {
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    if (!userData.age) {
+        errors.push('กรุณากรอกอายุ')
+    }
+    if (!userData.gender) {
+        errors.push('กรุณาเลือกเพศ')
+    }
+    if (!userData.interests) {
+        errors.push('กรุณาเลือกความสนใจ')
+    }
+    if (!userData.description) {
+        errors.push('กรุณากรอกคำอธิบาย')
+    }
+      return errors
+}
+    
 /*
 app.get('/testdbnew', async (req, res) => {
 
@@ -52,17 +77,26 @@ app.post('/users', async (req, res) => {
     
     
     try{
-        let user = req.body;    
+        let user = req.body;
+        const errors = validateData(user)
+        if(errors.length > 0) {
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }    
         const results = await conn.query('INSERT INTO users SET ?', user)
         res.json({
             message: 'Create user successfully',    
             data: results[0]
         })
     }catch(error){
+        const errorMessage = error.message || 'somnething went wrong'
+        const errors = error.errors || []
         console.error('error: ', error.message)
         res.status(500).json({
-            message: 'something went wrong',
-            errorMessage: error.message
+            message: errorMessage,
+            errors: errors
         })
     }
 })
